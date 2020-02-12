9f2a2cb0b04b95c2d5f1a3333aa97913ea1d0de1
ramramhub <kapoor3000@yahoo.com>
2020-02-12 15:04:32 -0600

    interactive scene created

--- image-demo/sketch.js
+++ image-demo/sketch.js
@@ -6,6 +6,9 @@
 // - describe what you did to take this project "above and beyond"
 
 let dinosaur;
+let w = 250;
+let h = 100;
+let d = 20;
 
 function preload() {
   dinosaur = loadImage("assets/spinosaurus_dinosaur.jpg");
@@ -15,7 +18,20 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+function mouseWheel(event) {
+  if (event.deltaY < 0) {
+    h = h + h * 0.1;
+    w = w + w * 0.1;
+  }
+
+  else {
+    h = h - h * 0.1;
+    w = w - w * 0.1;
+  }
+}
+
 function draw() {
   background(220);
-  image(dinosaur, mouseX, mouseY, 500, 100);
+  image(dinosaur, mouseX - w/2, mouseY - h/2, w, h);
 }
+
