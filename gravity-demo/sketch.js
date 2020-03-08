80787a18d6afcdaae26d0e8d08eed9e42f20f025
ramramhub <60943804+ramramhub@users.noreply.github.com>
2020-03-08 00:08:53 -0600

    added scroll wheel functionality

--- gravity-demo/sketch.js
+++ gravity-demo/sketch.js
@@ -1,9 +1,13 @@
-// Project Title
-// Your Name
-// Date
+// Interactive Scene
+// Ramit Kapoor
+// March 9th, 2020
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - added smooth and coherent physics system in project
+// - added WASD controls for movement
+// - demonstrated understanding in state variables to improve code efficiency
+// - added scroll wheel interactivity with scene
+// - demonstrated knowledge on object rotation
 
 let x, y;
 let dx, dy;
@@ -17,6 +21,7 @@ let rectColourB = 0;
 let rectColourG = 0;
 
 let movingUp, movingDown, movingLeft, movingRight = false;
+let scalar = 1;
 let s = 25;
 
 
@@ -24,7 +29,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   rectMode(CENTER);
-  angleMode(DEGREES);
+  angleMode(DEGREES); 
 
   x = width/2;
   y = height/2;
@@ -106,7 +111,7 @@ function drawObject() {
   push();
   translate(x, y);
   rotate(degree);
-  rect(0, 0, s*2, s*2);
+  rect(0, 0, scalar * s * 2, scalar * s * 2);
   pop();
 }
 
@@ -131,26 +136,26 @@ function movementSetup() {
 }
 
 function checkIfObjectInWindow() {
-  if (y > windowHeight - s) {
+  if (y > windowHeight - s * scalar) {
     dy *= -0.1;
-    y = windowHeight - s;
+    y = windowHeight - s * scalar;
   }
 
-  if (y < 0 + s) {
+  if (y < 0 + s * scalar) {
     dy *= -0.9;
-    y = 40;
+    y = 40 * scalar;
     randomizeRectColour();
   }
 
-  if (x < 0 + s/2) {
+  if (x < 0 + s/2 * scalar) {
     dx *= -0.9;
-    x = 20;
+    x = 20 * scalar;
     randomizeRectColour();
   }
 
-  if (x > windowWidth - s/2) {
+  if (x > windowWidth - s/2 * scalar) {
     dx *= -0.9;
-    x = windowWidth - 20;
+    x = windowWidth - 20 * scalar;
     randomizeRectColour();
   }
 
@@ -160,4 +165,14 @@ function randomizeRectColour() {
   rectColourR = random(0, 255);
   rectColourG = random(0, 255);
   rectColourB = random(0, 255);
+}
+
+function mouseWheel(scaleRect) {
+  if (scaleRect.deltaY > 0 && scalar < 5) {
+    scalar *= 1.1;
+  }
+
+  else if (scaleRect.deltaY < 0 && scalar > 0.05) {
+    scalar *= 0.9;
+  }
 }
